3e22006c4a31622338c0227b492fb34f59f40160
Kalaiarasan K <52280933+KalaiarasanK@users.noreply.github.com>
2020-01-03 10:43:11 +0530

    Create perfect square or not.py

---
+++ perfect square or not.py
@@ -0,0 +1,9 @@
+#Given 2 numbers N,M. Print 'yes' if their product is a perfect square else print 'no'.
+
+import math
+a = map(int,input().split())
+root = math.sqrt(a)
+if == a:
+    print("yes")
+else:
+    print("no")
