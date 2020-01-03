5dcd6b41aecb220df024a9e8875c207ab8344aba
Kalaiarasan K <52280933+KalaiarasanK@users.noreply.github.com>
2020-01-03 10:53:15 +0530

    Create smallest number possible using the digits of number..py

---
+++ smallest number possible using the digits of number..py
@@ -0,0 +1,8 @@
+#You are given a number ‘n’. Your task is to create the smallest number possible using the digits of number.
+
+numbers=str(input())
+a=[]
+for i in a:
+	a.append(i)
+m=sorted(numbers) 
+print(*m,sep="") 
